3af241a2c644e1421827879cc39b519c2f41e64c
Henywealth12 <eniolaomotayo323@gmail.com>
2026-04-06 07:43:57 +0100

    Create app.module.ts

---
+++ backend/src/jobs/dto/backend/src/jobs/backend/src/jobs/backend/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { JobsController } from './jobs/jobs.controller';
+import { JobsService } from './jobs/jobs.service';
+
+@Module({
+  controllers: [JobsController],
+  providers: [JobsService],
+})
+export class AppModule {}
